db329837988b52fdbf437681d7f7bf2ae1e7c0a1
David Christoffersen <d@vidc.dk>
2021-11-09 19:02:49 -0500

    More react router

--- Frontend/yir-frontend/src/App.js
+++ Frontend/yir-frontend/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import Main from './components/Main/Main';
 
-    </div>
+function App() {
+
+  /* User oriented states: */
+  const [user, setUser] = useState("true")
+  const [userObj, setUserObj] = useState({});
+
+
+  return (
+    <main className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/">
+            < Main />
+          </Route>
+          {/* protected route */}
+          <Route path="*" >
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <h1> Year in Review</h1>
+    </main>
   );
 }
 
---
+++ Frontend/yir-frontend/src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <h2> hello from Main</h2>
+    );
+}
+export default Main;

d171e511215aebf3e2c282f62010432a603b12df
David Christoffersen <d@vidc.dk>
2021-11-11 13:30:47 -0500

    login simple

--- Frontend/yir-frontend/src/components/LogOnIn/LogOnIn.js
+++ Frontend/yir-frontend/src/components/LogOnIn/LogOnIn.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 
 const LogOnIn = (props) => {
     const [user, signup] = props;
+
+    const [isSignup, setIsSignup] = useState(false);
+
+    const toggleSign = () => {
+        setIsSignup(!isSignup);
+    }
+
+    const btnText = isSignup ? "Signup" : "SignIn";
+
+
+
     return (
         <p> {signup} </p>
+        <button  onClick={toggleSign}> {btnText} </button>
     )
 }
 

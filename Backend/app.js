17ae12e998f5d80404e03aa563d1b85b3d951223
David Christoffersen <d@vidc.dk>
2021-11-17 13:11:41 -0500

    Adding some backend magic

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 
+const { PORT = 3000 } = process.env;
+const { }
+
+
 
 app.get("/url", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+/* #TODO delete the console.log...not allowed */
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
 });

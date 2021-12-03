e09da82d8a2834415d332ec58475866c9dc0c993
David Christoffersen <d@vidc.dk>
2021-12-03 09:43:37 -0500

    API stuff

--- Frontend/yir-frontend/src/utils/api.js
+++ Frontend/yir-frontend/src/utils/api.js
@@ -1,8 +1,8 @@
-import REACT_APP_KEY from '../../env.env';
+const { REACT_APP_API } = process.env
 
 class Api {
     constructor() {
-        this._MainUrl = API_MAIN;
+        this._MainUrl = REACT_APP_API;
     }
 
     signin(email, password) {
@@ -26,9 +26,47 @@ class Api {
             }).catch((res) => Promise.reject(new Error(`${res.status} : ${res.message}`)));
     }
 
+    getCurrentUser(token) {
+        const userUrl = this._MainUrl.concat('/users/me');
+        return fetch(userUrl, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                return Promise.reject(new Error(`${res.status} : ${res.message}`));
+            }).catch((res) => Promise.reject(new Error(`${res.status} : ${res.message}`)));
+    }
+
+    signup(email, password, name) {
+        const userUrl = this._MainUrl.concat('/signup');
+        return fetch(userUrl, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password, name }),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw new Error(`${res.status} : ${res.message}`);
+            })
+            .catch((err) => {
+                throw new Error(`${err.status} : ${err.message}`);
+            });
+    }
 
 }
 
-const api = new Api('http://api.nomad.students.nomoreparties.site');
+const api = new Api();
 
 export default api;
